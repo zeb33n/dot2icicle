dcc1add954277685e7a3df55be84cf845ecb5523
zeb <bondzeb@gmail.com>
2025-07-14 14:43:06 +0100

    working version

--- icicle.py
+++ icicle.py
@@ -2,11 +2,8 @@ from collections import defaultdict
 from pathlib import Path
 import sys
 import plotly.express as px
-from dataclasses import field, dataclass
-from copy import deepcopy
 
 
-@dataclass
 class Node:
     def __init__(self, name: str):
         self.name: str = name
@@ -37,59 +34,6 @@ def get_nodes() -> Node:
     return nodes[next(iter(node_names))]
 
 
-# def expand_nodes(nodes: dict[str, Node]) -> dict[str, Node]:
-#     roots = [name for name, node in nodes.items() if len(node.parents) == 0]
-#     if len(roots) > 1:
-#         raise Exception("Too many root too many many root")
-#     root = roots[0]
-
-#     seen = set()
-
-#     def rec(node_name: str):
-#         if node_name in seen:
-#             return
-#         seen.add(node_name)
-#         if len(nodes[node_name].parents) > 1:
-#             child = nodes.pop(node_name)
-#             new_nodes = {
-#                 parent_name: node_name + str(i)
-#                 for i, parent_name in enumerate(child.parents)
-#             }
-#             children = []
-#             for parent_name, child_name in new_nodes.items():
-#                 nodes[child_name] = Node(child.children, {parent_name})
-#                 nodes[parent_name].children -= {node_name}
-#                 nodes[parent_name].children.add(child_name)
-#                 children.append(child_name)
-#             for child_name in children:
-#                 ccs = deepcopy(nodes[child_name].children)
-#                 for child_child in ccs:
-#                     print(len(ccs))
-#                     new_names = list(new_nodes.values())
-#                     nodes[child_child].parents = (
-#                         nodes[child_child].parents - {node_name}
-#                     ).union(new_names)
-#                     print(node_name)
-#                     print(child_child)
-#                     print(nodes[child_child].parents)
-#                     rec(child_child)
-#         else:
-#             for child in nodes[node_name].children:
-#                 rec(child)
-
-#     rec(root)
-
-#     return nodes
-
-
-# while no nodes have
-# loop through nodes
-# if num parents > 1
-# pop current node
-# make copies of nodes with ids for num of parents
-#
-
-
 def get_data_for_plot(root: Node) -> dict[str, list[str]]:
     data = {"child": [], "parent": [""]}
     buckets = defaultdict(lambda: 0)
@@ -111,14 +55,7 @@ def plot():
     node = get_nodes()
     data = get_data_for_plot(node)
 
-    for child, parent in zip(data["child"], data["parent"]):
-        print(child)
-        print(parent)
-        print("")
-
-    print(data["child"])
-    print(data["parent"])
-    fig = px.icicle(data, names="child", parents="parent")
+    fig = px.treemap(data, names="child", parents="parent")
     fig.update_traces(root_color="lightgrey")
     fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
     fig.show(renderer="browser")
